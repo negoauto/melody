ead960ce058441d5cb28c891ef7a7b6f501d1a2c
negoauto <132526024+negoauto@users.noreply.github.com>
2023-05-04 03:33:05 +0300

    Add files via upload

---
+++ d.py
@@ -0,0 +1,22 @@
+import os
+
+if not os.path.isfile("serias.txt"):
+    print("created serias.txt")
+    with open("serias.txt", "w"):
+        pass
+
+if not os.path.isfile("movies.txt"):
+    print("created movies.txt")
+    with open("movies.txt", "w"):
+        pass
+
+if not os.path.isfile("links.txt"):
+    print("created links.txt")
+    with open("links.txt", "w"):
+        pass
+
+if not os.path.isfile("eposit.txt"):
+    print("created eposit.txt")
+    with open("eposit.txt", "w"):
+        pass
+print("all files found")
